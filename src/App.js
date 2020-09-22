63ada610e204167b2c0328b312198599090cdfd1
buna_branislav <branislav.buna@spiressl.com>
2020-09-22 10:27:12 +0100

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <h1 className="App-heading">Where in the world ?</h1>
+          <div className="dark_mode">
+              <FontAwesomeIcon icon={faMoon} />
+              <span className="dark_mode_text">Dark Mode</span>
+          </div>
       </header>
     </div>
   );
