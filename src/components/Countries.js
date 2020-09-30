72550f4d263998549d7d25ba52fe8df3ad00a28e
GlobalGraphic <graphicglobal67@gmail.com>
2020-09-30 12:17:33 +0100

    used axios to fetch all the countries from API

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../App.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import axios from 'axios';
 
 const Countries = () => {
+
+    const [country, setCountry] = useState([]);
+
+    useEffect(() => {
+       getCountries();
+    }, []);
+
+    const getCountries = async () => {
+        const result = await axios(
+            'https://restcountries.eu/rest/v2/all'
+        );
+        setCountry(result.data);
+        console.log(result.data);
+    }
+
     return (
         <Container fluid>
             <Row>
-                <Col>1 of 1</Col>
-                <Col>1 of 1</Col>
-                <Col>1 of 1</Col>
-                <Col>1 of 1</Col>
+                {country.map(function(item,key){
+                   return (
+                       <Col id={key}>
+                           <Image className='country_img' src={item.flag} alt={item.alpha2code}/>
+                           <p>{item.name}</p>
+                       </Col>
+                   );
+                })};
             </Row>
         </Container>
     );
